df1fe531f01c4f4a6018d6d6a032875358f89b23
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-09-11 13:03:20 -0700

    ✨ feat: ButtonSend component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { ButtonSend } from "./components/ButtonSend/ButtonSend";
 import { SuggestionBox } from "./components/SuggestionBox/SuggestionBox";
 
 function App() {
   return (
     <>
+      <ButtonSend />
       <SuggestionBox />
     </>
   );
--- src/components/ButtonSend/ButtonSend.tsx
+++ src/components/ButtonSend/ButtonSend.tsx
@@ -1,3 +1,9 @@
+import { SendIcon } from "../Icons";
+
 export const ButtonSend = () => {
-  return <button></button>;
+  return (
+    <button className="bg-sky-900 rounded-full p-5 text-white rotate-[270deg] hover:bg-sky-800 transition-colors">
+      <SendIcon />
+    </button>
+  );
 };
