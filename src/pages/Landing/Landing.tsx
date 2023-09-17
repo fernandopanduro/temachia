552cf5bf468cbf288fcdcc26754ce880cf6eec0f
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-09-17 12:32:00 -0700

    🌈 style(Change copys):

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,11 +1,10 @@
+import { Link } from "react-router-dom";
 import { Footer } from "../../components/Footer/Footer";
-import { Header } from "../../components/Header/Header";
 
 export const Landing = () => {
   return (
     <div>
-      <Header />
-      <div>
+      <div className="dark:bg-gray-900">
         <header className=" absolute inset-x-0 top-0 z-50">
           <nav
             className="flex items-center justify-between p-6 lg:px-8"
@@ -20,124 +19,33 @@ export const Landing = () => {
                 />
               </a>
             </div>
-            <div className="flex lg:hidden">
-              <button
-                type="button"
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                <span className="sr-only text-white">Open main menu</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true">
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-              </button>
+            <div className="flex gap-x-4 lg:gap-x-12">
+              <Link
+                to={"/"}
+                className="text-sm font-semibold leading-6 text-white">
+                Home
+              </Link>
+              <a
+                href="#planes"
+                className="text-sm font-semibold leading-6 text-white">
+                Planes
+              </a>
+              <Link
+                to={"/contact"}
+                className="text-sm font-semibold leading-6 text-white">
+                Contacto
+              </Link>
             </div>
-            <div className="hidden lg:flex lg:gap-x-12">
-              <a
-                href="#"
+            <div className="lg:flex lg:flex-1 lg:justify-end">
+              <Link
+                to={"/auth/login"}
                 className="text-sm font-semibold leading-6 text-white">
-                Product
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white">
-                Features
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white">
-                Marketplace
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white">
-                Company
-              </a>
-            </div>
-            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white">
-                Log in <span aria-hidden="true">&rarr;</span>
-              </a>
+                Entrar <span aria-hidden="true">&rarr;</span>
+              </Link>
             </div>
           </nav>
-          <div className="lg:hidden" role="dialog" aria-modal="true">
-            <div className="fixed inset-0 z-50"></div>
-            <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only text-white">Your Company</span>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt=""
-                  />
-                </a>
-                <button
-                  type="button"
-                  className="-m-2.5 rounded-md p-2.5 text-gray-700">
-                  <span className="sr-only text-white">Close menu</span>
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-50 text-white">
-                      Product
-                    </a>
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50">
-                      Features
-                    </a>
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50">
-                      Marketplace
-                    </a>
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50">
-                      Company
-                    </a>
-                  </div>
-                  <div className="py-6">
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50">
-                      Log in
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </header>
-        <div className="dark:bg-gray-900 relative isolate px-6 pt-14 lg:px-8">
+        <div className="bg-gray-900 relative isolate px-6 pt-14 lg:px-8">
           <div
             className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
             aria-hidden="true">
@@ -149,35 +57,26 @@ export const Landing = () => {
               }}></div>
           </div>
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-white">
-                Announcing our next round of funding.{" "}
-                <a href="#" className="font-semibold text-indigo-600">
-                  <span className="absolute inset-0" aria-hidden="true"></span>
-                  Read more <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div>
-            </div>
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl dark:text-white">
-                Data to enrich your online business
+                Temach IA
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-white">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+                Conquista a tu pareja ideal entre mujeres excepcionales con
+                estrategias personalizadas potenciadas por inteligencia
+                artificial.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="#"
+                <Link
+                  to={"/auth/register"}
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                  Get started
-                </a>
-                <a
-                  href="#"
+                  Registrar
+                </Link>
+                <Link
+                  to={"/auth/login"}
                   className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
+                  Entrar <span aria-hidden="true">→</span>
+                </Link>
               </div>
             </div>
           </div>
